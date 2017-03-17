ebad679c7282131fb5bd66108784995cc1036d44
Preston Matheson <pmphoto123@gmail.com>
2017-03-17 10:58:24 -0600

    added clarification on the fibbonacci exercise and an interactive daily special method to choose a daily special based off of the course and menu items

--- january/7.rb
+++ january/7.rb
@@ -3,11 +3,22 @@ require 'rspec'
 menu = {
   'appetizers': ['Chips', 'Quesadillas', 'Flatbread'],
   'entrees': ['Steak', 'Chicken', 'Lobster'],
-  'dessers': ['Cheesecake', 'Cake', 'Cupcake']
+  'desserts': ['Cheesecake', 'Cake', 'Cupcake']
 }
 
+# first solution I came up with
+
 def daily_special hash
+  array = hash.values
+  array_2 = array.sample
+  array_3 = array_2.sample
+  
 end
+
+# def daily_special hash
+#   array = hash.valuse
+#   array_2 = array.flatten.sample
+# end
 
 describe 'Nested hash element selector' do
   it 'selected a random element from the set of nested arrays' do
@@ -15,3 +26,14 @@ describe 'Nested hash element selector' do
   end
 end
 
+# take course and return a random menu item in that course category
+
+
+# def create_special(hash, course)
+#   hash.assoc course
+#   key_hash = hash.select { |k,v| k == course }
+#   special_hash = key_hash.values
+#   special_hash_2 = special_hash.flatten.sample 
+# end 
+
+# p create_special(menu, 'appetizers')
