cddc190ecd4eb7ded0e895cf3fcb4e036442d59a
Preston Matheson <pmphoto123@gmail.com>
2017-03-20 09:30:05 -0600

    coding exercise from exercism.io 'bob' the lackadaisical teenager

---
+++ bob.rb
@@ -0,0 +1,29 @@
+# Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+# Bob answers 'Sure.' if you ask him a question.
+
+# He answers 'Whoa, chill out!' if you yell at him.
+
+# He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+# He answers 'Whatever.' to anything else.
+
+def bob
+  print "Please say something to bob."
+  words = gets.chomp!
+        
+  if words == words.upcase
+    puts 'Whoa, chill out!'
+
+  elsif words.downcase == 'bob'
+    puts 'Fine. Be that way!'
+      
+  elsif words.split(//).last == "?"
+    puts 'Sure.'      
+
+  else
+    puts 'Whatever'
+  end
+end
+
+bob
