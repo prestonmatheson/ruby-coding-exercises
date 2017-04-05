3edc47b9d4cf11bb98353fea9988138a49af778c
Preston Matheson <pmphoto123@gmail.com>
2017-04-05 11:30:03 -0600

    refactored pig latin and fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,21 +1,19 @@
 # Write a program that takes an argument n and returns the fibonacci value of that position. The fibonacci sequence is 0, 1, 1, 2, 3, 5, 8, 13, 21... So fib(5) should return 5 and fib(6) should return 8.
 
-def fibonacci( n )
-  return  n  if ( 0..1 ).include? n
-  ( fibonacci( n - 1 ) + fibonacci( n - 2 ) )
-end
+# def fibonacci( n )
+#   return  n  if ( 0..1 ).include? n
+#   ( fibonacci( n - 1 ) + fibonacci( n - 2 ) )
+# end
 
 
 
 
 #this way makes more sense to me atm!
 
-# def fib(a, b = {})
-#   if a == 0 || a == 1
-#     return a
-#   end
-#   b[a] ||= fib(a-1, b) + fib(a-2, b)
-# end
+def fib(a, b = {})
+  return a if a == 0 || a == 1
+  b[a] ||= fib(a-1, b) + fib(a-2, b)
+end
 
-# p fib(2000)
+p fib(100)
 
--- pig_latin.rb
+++ pig_latin.rb
@@ -1,8 +1,36 @@
-require 'pry'
-
 class String
+
+  def syllable_count # this method is written by Elisabeth Hendrickson, Quality Tree Software, Inc. and used here for educational purposes only
+    consonants = "bcdfghjklmnpqrstvwxz"
+    vowels = "aeiouy"
+    processed = self.downcase
+    suffix_bonus = 0
+    #puts "*** 0 #{processed}"
+    if processed.match(/ly$/)
+      suffix_bonus = 1
+      processed.gsub!(/ly$/, "")
+    end
+    if processed.match(/[a-z]ed$/)
+      # Not counting "ed" as an extra symbol. 
+      # So 'blessed' is assumed to be said as 'blest'
+      suffix_bonus = 0 
+      processed.gsub!(/ed$/, "")
+    end
+    #puts "*** 1 #{processed}"
+    processed.gsub!(/iou|eau|ai|au|ay|ea|ee|ei|oa|oi|oo|ou|ui|oy/, "@") #vowel combos
+    #puts "*** 2 #{processed}"
+    processed.gsub!(/qu|ng|ch|rt|[#{consonants}h]/, "=") #consonant combos
+    #puts "*** 3 #{processed}"
+    processed.gsub!(/[#{vowels}@][#{consonants}=]e$/, "@|") # remove silent e
+    #puts "*** 4 #{processed}"
+    processed.gsub!(/[#{vowels}]/, "@") #all remaining vowels will be counted
+    #puts "*** 5 #{processed}"
+    return processed.count("@") + suffix_bonus
+  end
+  
+
   def pig_latin
-    if self[0,1].downcase.include?("a") || self[0,1].downcase.include?("e") ||self[0,1].downcase.include?("i") || self[0,1].downcase.include?("o") || self[0,1].downcase.include?("u")
+    if (self[0,1].downcase.include?("a") || self[0,1].downcase.include?("e") ||self[0,1].downcase.include?("i") || self[0,1].downcase.include?("o") || self[0,1].downcase.include?("u")) && self.syllable_count == 1
       self << "ay"
     else 
       self << self[0,1]
@@ -12,7 +40,7 @@ class String
   end
 end
 
-test = %w[the cow jumped over the blue moon test is ten times better]
+test = %w[you idiot jumped over the blue moon test is ten times better]
 
 test.each do |x|
   x.pig_latin
