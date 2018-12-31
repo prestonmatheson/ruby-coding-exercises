f15c317203dbd6f927eeef91f6358687bbc81313
Preston Matheson <pmphoto123@gmail.com>
2018-12-31 15:48:47 -0800

    added a html parser to the hash class

--- december/30.rb
+++ december/30.rb
@@ -1,5 +1,13 @@
 require 'rspec'
 
+class Hash
+  def param_converter
+    html_safe = ""
+    self.map { |x, y| html_safe << (x.to_s + "=" + y + "&")}
+    return html_safe.chomp("&")
+  end
+end
+
 describe 'HTML Param Converter' do
   it 'Adds an HTML param converter to the Hash class' do
     hash = { :topic => "baseball", :team => "astros" }
