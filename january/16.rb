ce3c5d041f605577ca0fefba673cf3c667479bf5
Preston Matheson <pmphoto123@gmail.com>
2019-01-19 10:02:53 -0800

    rotating the indexes of elements until the given number is at the end of an array challenge

--- january/16.rb
+++ january/16.rb
@@ -1,6 +1,15 @@
 require 'rspec'
 
 def array_rotation arr, num
+  loop do 
+    unless arr.last == num
+      arr.unshift(arr.last)
+      arr.pop
+    else
+      return arr
+      break
+    end
+  end
 end
 
 describe 'Array index change' do
@@ -9,3 +18,7 @@ describe 'Array index change' do
   end
 end
 
+# 1 2 3 4 5
+# 5 1 2 3 4
+# 4 5 1 2 3
+# 3 4 5 1 2
