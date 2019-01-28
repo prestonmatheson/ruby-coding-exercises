70624b44de37b8f71ac9958f15d63aacd2dc3aa3
Preston Matheson <pmphoto123@gmail.com>
2019-01-27 18:24:35 -0800

    generating an image tag that allows for tags to be given optionally

--- january/27.rb
+++ january/27.rb
@@ -1,5 +1,28 @@
 require 'rspec'
 
+def generate_tags keys, kv_pairs
+  # determine and generate tags for each arg and adding a space at the begining
+  tags = []
+  keys.each do |key|
+    tags << " #{key}='#{kv_pairs[key.to_sym]}'"
+  end
+  return tags
+end
+
+def image_tag path, optional_args=nil
+  str = "<img src='#{path}'"
+  if optional_args !=nil
+    keys = optional_args.keys
+    tags = generate_tags keys, optional_args
+    tags.each do |tag|
+      str += tag
+    end
+    str += ">"
+  else
+    str += ">"
+  end
+  return str
+end
 describe 'Cloned image_tag' do
   before do
     @image_path = "https://devcamp.com/some_pic.jpg"
