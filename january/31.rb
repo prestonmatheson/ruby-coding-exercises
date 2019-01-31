1860269edf6dbb68eb213da401b128913cc934aa
Preston Matheson <pmphoto123@gmail.com>
2019-01-31 12:08:42 -0800

    using %w to create an array of months

--- january/31.rb
+++ january/31.rb
@@ -1,5 +1,7 @@
 require 'rspec'
-
+def months
+  %w(January February March April May June July August September October November December)
+end
 describe 'Month generator' do
   it 'returns the full list of months for a year as an array' do
     expect(months).to eq(["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"])
