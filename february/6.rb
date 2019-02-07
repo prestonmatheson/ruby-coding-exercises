ca5abbf0f3020a8379c3cb110f3c184710e9699c
Preston Matheson <pmphoto123@gmail.com>
2019-02-06 16:11:46 -0800

    adding an rspec test to start practicing writing my own unit tests as well as solving the original problem

--- february/6.rb
+++ february/6.rb
@@ -6,6 +6,12 @@ def find_element arrs, element
   end
 end
 
+def find_jersey_number arrs, player
+  arrs.each do |arr|
+    return arr[0] if arr[1] == player
+  end
+end
+
 describe 'Find Element' do
   it 'returns an array from a nested array if the second element equals the queried element' do
     players = [
@@ -20,3 +26,16 @@ describe 'Find Element' do
   end
 end
 
+describe 'Find Jersey Number' do
+  it 'returns the jersey number given a player name' do
+    players = [
+      [27, 'Jose Altuve'],
+      [2,  'Alex Bregman'],
+      [1,  'Carlos Correa'],
+      [9,  'Marwin Gonzalez'],
+      [10, 'Yulieski Gurriel']
+    ]
+
+    expect(find_jersey_number(players, 'Carlos Correa')).to eq(1)
+  end
+end
