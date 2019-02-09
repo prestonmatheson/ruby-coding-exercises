9a686419f87c335e924098136f80b220ded85813
Preston Matheson <pmphoto123@gmail.com>
2019-02-09 15:55:10 -0800

    added two methods to the array class, one eliminates duplicates from an array, the other returns the numbers that have multiples in the array

--- february/8.rb
+++ february/8.rb
@@ -1,5 +1,29 @@
 require 'rspec'
 
+class Array
+  def remove_duplicates
+    parsed = []
+    self.each do |item|
+      unless parsed.include?(item)
+        parsed << item
+      end
+    end
+    parsed
+  end
+
+  def duplicates
+    duplicates = []
+    arr = []
+    self.each do |item|
+      if arr.include?(item) && !duplicates.include?(item)
+        duplicates << item
+      else
+        arr << item
+      end
+    end
+    duplicates
+  end
+end
 describe 'Duplicate removal' do
   it 'Removed duplicates from an array' do
     arr = [1, 3, 4, 1, 4]
@@ -7,3 +31,9 @@ describe 'Duplicate removal' do
   end
 end
 
+describe 'Identify multiples' do
+  it 'returns a list of numbers that have multiples' do
+    arr = [1, 3, 4, 1, 4]
+    expect(arr.duplicates).to eq([1, 4])
+  end
+end
